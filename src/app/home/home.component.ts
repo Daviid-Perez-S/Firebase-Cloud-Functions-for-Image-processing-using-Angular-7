391773122d6d52a78a5232960a975abb472fc90c
David Pérez S <playeando.97@gmail.com>
2019-07-29 09:19:18 -0500

    Cambios estéticos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
 import { AngularFirestore } from '@angular/fire/firestore'
 import { AngularFireStorage } from '@angular/fire/storage'
 import { finalize } from 'rxjs/operators'
@@ -13,202 +13,180 @@ import { Router, NavigationExtras } from '@angular/router'
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  ngOnInit() {
-  } 
-  @ViewChild("video")
+  
+  @ViewChild('video')
   public video: ElementRef;
-  @ViewChild("canvas")
+  @ViewChild('canvas')
   public canvas: ElementRef;
+  
+  public captures: any = [];
 
-  public captures:any = [];
-
-
-  analyzedPicture:any = null
-
-
-  file: any = null
-  fileInput = null
+  
+  analyzedPicture: any = null;
+  
+  
+  file: any = null;
+  fileInput = null;
   title = 'cloudFunctionsClient';
-  _file:any
+  _file: any;
   
-  uploadPercent: Observable<number> = null
-  downloadURL: Observable<string> = null
+  uploadPercent: Observable<number> = null;
+  downloadURL: Observable<string> = null;
   
-  __docRef:any = null
-
-
-  labels: any = []
-
+  __docRef: any = null;
+  
+  
+  labels: any = [];
+  
+  ngOnInit() {
+  }
   constructor(
     private firestorage: AngularFirestore,
     private storage: AngularFireStorage,
-    private functions:HttpClient,
+    private functions: HttpClient,
     private router: Router,
-    )
-    {}
-    
-    
-    verificar()
-    {
-      if(this._file != null)
-      {
-        this.upload()
-      }
-      else
-      {
-        alert('No has subido una imagen')
-      }
+  ) { }
+
+  verificar() {
+    if (this._file != null) {
+      this.upload()
     }
-    
-    onFileSelected(event)
-    {
-      if(event.target.files.length > 0) 
-      {
-        const reader = new FileReader()
-        reader.onload = (e:any) => {
-          this.file = e.target.result
-        }
-        this._file = event.target.files[0]
-        reader.readAsDataURL(event.target.files[0])
-        console.log(event.target.files[0])
-      }
+    else {
+      alert('No has subido una imagen')
     }
-    
-    getFileId(filePath:any)
-    {
-      if(filePath.includes('.JPG'))
-      {
-        return filePath.split('.JPG')[0]
-      }
-      else if(filePath.includes('.jpg'))
-      {
-        return filePath.split('.jpg')[0]
-      }
-      else if(filePath.includes('.png'))
-      {
-        return filePath.split('.png')[0]
-      }
-      else if(filePath.includes('.PNG'))
-      {
-        return filePath.split('.PNG')[0]
-      }
-      else
-      {
-        return 'invalid image'
-      }
+  }
+
+  onFileSelected(event) {
+    if (event.target.files.length > 0) {
+      const reader = new FileReader()
+      reader.onload = (e: any) => {
+        this.file = e.target.result;
+      };
+      this._file = event.target.files[0]
+      reader.readAsDataURL(event.target.files[0])
+      console.log(event.target.files[0])
     }
-    
-    upload()
-    {
-      const filePath = this._file.name
-      const fileId = this.getFileId(filePath)
-      if(fileId != 'invalid image')
-      {
-        const fileRef = this.storage.ref(filePath);
-        this.__docRef = fileRef
-        const task = this.storage.upload(filePath, this._file)
-        this.uploadPercent = task.percentageChanges()
-        
-        task.snapshotChanges().pipe(
+  }
+
+  getFileId(filePath: any) {
+    if (filePath.includes('.JPG')) {
+      return filePath.split('.JPG')[0]
+    }
+    else if (filePath.includes('.jpg')) {
+      return filePath.split('.jpg')[0]
+    }
+    else if (filePath.includes('.png')) {
+      return filePath.split('.png')[0]
+    }
+    else if (filePath.includes('.PNG')) {
+      return filePath.split('.PNG')[0]
+    }
+    else {
+      return 'invalid image'
+    }
+  }
+
+  upload() {
+    const filePath = this._file.name
+    const fileId = this.getFileId(filePath)
+    if (fileId != 'invalid image') {
+      const fileRef = this.storage.ref(filePath);
+      this.__docRef = fileRef
+      const task = this.storage.upload(filePath, this._file)
+      this.uploadPercent = task.percentageChanges()
+
+      task.snapshotChanges().pipe(
         finalize(
           () => this.downloadURL = fileRef.getDownloadURL()
-          )
-        ).subscribe()
-        let docRef = this.firestorage.collection("photos").doc(fileId)
-        docRef.snapshotChanges().subscribe(res => {
-          this.analyzedPicture = res.payload.data()
-          this.readTags(res.payload.data())
-        })
-        
-        this.uploadPercent = null
-      }
-      else
-      {
-        alert('La imágen no es válida')
-      }
-    }
+        )
+      ).subscribe()
+      let docRef = this.firestorage.collection('photos').doc(fileId)
+      docRef.snapshotChanges().subscribe(res => {
+        this.analyzedPicture = res.payload.data()
+        this.readTags(res.payload.data())
+      })
 
-    upload_from_webcam(image:any)
-    {
-      const filePath = image.name
-      const fileId = this.getFileId(filePath)
-      if(fileId != 'invalid image')
-      {
-        // const fileId = filePath.split('.JPG')[0]
-        const fileRef = this.storage.ref(filePath);
-        this.__docRef = fileRef
-        const task = this.storage.upload(filePath, this._file)
-        this.uploadPercent = task.percentageChanges()
-        
-        task.snapshotChanges().pipe(
+      this.uploadPercent = null
+    }
+    else {
+      alert('La imágen no es válida')
+    }
+  }
+
+  upload_from_webcam(image: any) {
+    const filePath = image.name
+    const fileId = this.getFileId(filePath)
+    if (fileId != 'invalid image') {
+      // const fileId = filePath.split('.JPG')[0]
+      const fileRef = this.storage.ref(filePath);
+      this.__docRef = fileRef
+      const task = this.storage.upload(filePath, this._file)
+      this.uploadPercent = task.percentageChanges()
+
+      task.snapshotChanges().pipe(
         finalize(
           () => this.downloadURL = fileRef.getDownloadURL()
-          )
-        ).subscribe()
-          
-        let docRef = this.firestorage.collection("photos").doc(fileId)
-        docRef.snapshotChanges().subscribe(res => {
-          this.analyzedPicture = res.payload.data()
-          this.readTags(res.payload.data())
-        })
-        
-        this.uploadPercent = null
-      }
-      else
-      {
-        alert('La imágen no es válida')
+        )
+      ).subscribe()
+
+      let docRef = this.firestorage.collection('photos').doc(fileId)
+      docRef.snapshotChanges().subscribe(res => {
+        this.analyzedPicture = res.payload.data()
+        this.readTags(res.payload.data())
+      })
+
+      this.uploadPercent = null
+    }
+    else {
+      alert('La imágen no es válida')
+    }
+  }
+
+
+  goToDetailsView(_url) {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        "analisis": JSON.stringify(this.analyzedPicture),
+        "image": JSON.stringify(_url)
       }
     }
 
+    this.router.navigate(["details"], navigationExtras)
+  }
 
-    goToDetailsView(_url)
-    {
-      let navigationExtras: NavigationExtras = {
-        queryParams: {
-          "analisis": JSON.stringify(this.analyzedPicture),
-          "image": JSON.stringify(_url)
-        }
-      }
-      
-      this.router.navigate(["details"], navigationExtras)
-    }
-
-    takePhoto()
-    {
-      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
-      {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-          this.video.nativeElement.srcObject = stream
-          this.video.nativeElement.play()
-          setTimeout(() => {
-            this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0,0, 800,600)
-            this._file = this.canvas.nativeElement.toDataURL('image/png')
-            console.log(this._file)
-            fetch(this._file)
+  takePhoto() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.video.nativeElement.srcObject = stream
+        this.video.nativeElement.play()
+        setTimeout(() => {
+          this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 800, 600)
+          this._file = this.canvas.nativeElement.toDataURL('image/png')
+          console.log(this._file)
+          fetch(this._file)
             .then(res => res.blob())
             .then(blob => {
               const file = new File([blob], "generated.jpg")
               this._file = file
               return this.upload_from_webcam(file)
             })
-            stream.getVideoTracks().forEach(function (track) {
-                track.stop();
-            })
-          }, 500);
-        })
-      }
+          stream.getVideoTracks().forEach(function (track) {
+            track.stop();
+          })
+        }, 500);
+      })
     }
+  }
 
-    readTags(tags:any)
-    {
-      console.log(tags.labels)
-      // yeah, has to be this way
-      setTimeout(() => {
-        const json_tags = tags.labels
-        console.log(json_tags)
-        this.labels = json_tags
-        return Promise.resolve(this.labels)
-      }, 1000);
-    }
+  readTags(tags: any) {
+    console.log('Tags: ', tags.labels)
+    // yeah, has to be this way
+    setTimeout(() => {
+      const json_tags = tags.labels
+      console.log('Tags: ', json_tags)
+      this.labels = json_tags
+      return Promise.resolve(this.labels)
+    }, 5000);
+  }
 
 }
